f0c599fd1c677799b970736f5b3f52f285137954
hulucat <wz@hulucat.net>
2016-04-06 16:58:26 +0800

    dev

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 Route::group(['middleware' => ['web']], function () {
-	//
+
+	//消息回调
 	Route::get('corp/msg', 'hulucat\wechat-corp\CorpController@msg');
 	Route::get('corp/oauth2', 'hulucat\wechat-corp\CorpController@oauth2');
 });
